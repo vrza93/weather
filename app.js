10d1c94bb1c8fbda1bb74af999f54433d08bd244
vrza93 <137451859+vrza93@users.noreply.github.com>
2023-10-20 20:27:07 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+const apiKey = '39bd64552b9427b0f05e02e0d253a1f5';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
+const weatherIcon = document.querySelector('.weather-icon');
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if(response.status == 404){
+        document.querySelector('.error').style.display = 'block';
+        document.querySelector('.weather').style.display = 'none';
+    } else{
+        var data = await response.json();
+
+document.querySelector('.city').innerHTML = data.name;
+document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '&deg;C';
+document.querySelector('.humidity').innerHTML = data.main.humidity + '&percnt;';
+document.querySelector('.wind').innerHTML = data.wind.speed + 'km/h';
+document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + city + "')";
+
+if(data.weather[0].main == 'Clouds'){
+weatherIcon.src = 'images/clouds.png';
+}
+else if(data.weather[0].main == 'Clear'){
+weatherIcon.src = 'images/clear.png';
+}
+else if(data.weather[0].main == 'Rain'){
+weatherIcon.src = 'images/rain.png';
+}
+else if(data.weather[0].main == 'Drizzle'){
+weatherIcon.src = 'images/drizzle.png';
+}
+else if(data.weather[0].main == 'Mist'){
+weatherIcon.src = 'images/mist.png';
+}
+
+document.querySelector('.weather').style.display = 'block';
+document.querySelector('.error').style.display = 'none';
+
+    }
+
+
+};
+
+
+searchBtn.addEventListener('click', () => {
+    checkWeather(searchBox.value);
+});
+document.querySelector('.search input').addEventListener('keyup', function(event){
+    if (event.key == 'Enter') {
+        checkWeather(searchBox.value);
+    }
+    
+});
